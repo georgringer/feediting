33910a4a6e90464bddbdaa2e3757e4eb3d1efdf1
chris <typo3@mediadreams.org>
2024-04-18 21:20:25 +0200

    [FEATURE] Differentiate between tt_content and page records by setting a different color for the edit pen of pages
    Use the secondary blue from the TYPO3 styleguide for pages records

--- Classes/Edit/EditPanel.php
+++ Classes/Edit/EditPanel.php
@@ -100,13 +100,13 @@ class EditPanel
         $identifier = 'trigger' . md5(json_encode($data));
         $elementInformation = '<div class="tx-feediting-type">' . htmlspecialchars(implode(LF, $infos)) . '[<span>' . $this->recordId . '</span>]</div>';
         $panel = '
-<div class="popover-container">
+<div class="popover-container ' . $this->tableName . '">
   <button class="feediting-popover-trigger" data-position="top" data-popover-target="popover-' . $identifier . '">
   <img src="' . htmlspecialchars(PathUtility::getPublicResourceWebPath('EXT:feediting/Resources/Public/Icons/pen-solid.svg')) . '">
 </button>
 
   <template data-popover="popover-' . $identifier . '">
-    <div class="tx-feediting-panel">'
+    <div class="tx-feediting-panel ' . $this->tableName . '">'
             . $elementInformation
             . '<div class="tx-feediting-actions">' . implode(LF, $data) . '</div>'
             . '</div>
